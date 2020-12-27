085b6cf75e4a0adf55c2cdd24692ce8b32fc3533
Tomasz Sterna <tomek@sterna.link>
2020-12-27 22:25:48 +0100

    Split to separate packages

--- bevy_qml/src/main.rs
+++ bevy_qml/src/main.rs
@@ -1,9 +1,8 @@
-use bevy_qml::*;
-use pest::Parser;
+use qml_parser::*;
 use std::fs;
 
 fn main() -> std::result::Result<(), BoxError> {
-    let unparsed_file = fs::read_to_string("res/basic.qml")?;
+    let unparsed_file = fs::read_to_string("bevy_qml/res/basic.qml")?;
     let mut file = QMLParser::parse(Rule::qml, &unparsed_file)?;
     let file = file.next().unwrap(); // get and unwrap the `file` rule; never fails
 
--- qml-parser/src/lib.rs
+++ qml-parser/src/lib.rs
@@ -1,6 +1,8 @@
 #[macro_use]
 extern crate pest_derive;
 
+pub use pest::Parser;
+
 /// Type-erased errors.
 pub type BoxError = std::boxed::Box<dyn std::error::Error + std::marker::Send + std::marker::Sync>;
 
