670b701aa61470d48f54eebdf98cd9d08d875b89
Tomasz Sterna <tomek@sterna.link>
2020-12-28 14:24:24 +0100

    Process QML Object type

--- bevy_qml/src/main.rs
+++ bevy_qml/src/main.rs
@@ -1,13 +1,37 @@
-use qml_parser::QMLParser;
+use qml_parser::{Generator, QMLParser};
 use std::fs;
 
 pub type BoxError = std::boxed::Box<dyn std::error::Error + std::marker::Send + std::marker::Sync>;
+
+#[derive(Debug, Default)]
+struct UiGenerator {
+    created: bool,
+}
+
+impl Generator for UiGenerator {
+    fn done(&mut self) {
+        self.created = true;
+        println!("Done");
+    }
+}
+
+impl Drop for UiGenerator {
+    fn drop(&mut self) {
+        if !self.created {
+            self.done();
+        }
+    }
+}
 
 fn main() -> std::result::Result<(), BoxError> {
     let file = fs::read_to_string("bevy_qml/res/basic.qml")?;
 
     let mut parser = QMLParser::new();
-    parser.register_import("BevyUi", "0.4");
+    parser.register_import(
+        "BevyUi",
+        "0.4",
+        Box::new(|_name: &str| Some(Box::new(UiGenerator::default()))),
+    );
     parser.process(file.as_str())?;
 
     Ok(())
--- qml-parser/src/lib.rs
+++ qml-parser/src/lib.rs
@@ -1,8 +1,9 @@
 #[macro_use]
 extern crate pest_derive;
 
-use pest::Parser;
+use pest::{error, iterators::Pair, Parser};
 use std::collections::HashMap;
+use std::fmt::Debug;
 
 #[derive(Parser)]
 #[grammar = "qml.pest"]
@@ -11,9 +12,15 @@ struct QMLPest;
 #[cfg(test)]
 mod tests;
 
+pub type GeneratorFn = dyn Fn(&str) -> Option<Box<dyn Generator>>;
+
 pub struct QMLParser {
-    imports: HashMap<String, HashMap<String, ()>>,
-    in_scope: Vec<()>,
+    imports: HashMap<String, HashMap<String, Box<GeneratorFn>>>,
+    in_scope: Vec<Box<GeneratorFn>>,
+}
+
+pub trait Generator: Drop + Debug {
+    fn done(&mut self);
 }
 
 impl QMLParser {
@@ -24,24 +31,26 @@ impl QMLParser {
         }
     }
 
-    pub fn register_import<S: Into<String>>(&mut self, name: S, version: S) {
+    pub fn register_import<S: Into<String>>(&mut self, name: S, version: S, gen: Box<GeneratorFn>) {
         let import = self
             .imports
             .entry(name.into())
             .or_insert_with(|| HashMap::new());
-        let version_import = import.entry(version.into()).or_insert_with(|| ());
-        *version_import
+        import.insert(version.into(), gen);
     }
 
-    pub fn process(&mut self, data: &str) -> Result<(), pest::error::Error<Rule>> {
+    pub fn process(&mut self, data: &str) -> Result<(), error::Error<Rule>> {
         self.in_scope.clear();
 
         let mut parsed = QMLPest::parse(Rule::qml, data)?;
 
         for pair in parsed.next().unwrap().into_inner() {
             match pair.as_rule() {
-                Rule::import | Rule::object => {
-                    println!("{:?}", pair.as_rule());
+                Rule::import => {
+                    self.process_import(pair)?;
+                }
+                Rule::object => {
+                    self.process_object(pair)?;
                 }
                 Rule::pragma | Rule::EOI => {}
                 _ => {
@@ -51,5 +60,50 @@ impl QMLParser {
         }
 
         Ok(())
+    }
+
+    fn process_import(&mut self, pair: Pair<Rule>) -> Result<(), error::Error<Rule>> {
+        let mut pairs = pair.into_inner();
+        let name = pairs.next().unwrap();
+        let version = pairs.next().unwrap();
+
+        if let Some(import) = self.imports.get_mut(name.as_str()) {
+            if let Some(import) = import.remove(version.as_str()) {
+                self.in_scope.push(import);
+                Ok(())
+            } else {
+                Err(error::Error::new_from_span(
+                    error::ErrorVariant::<Rule>::CustomError {
+                        message: "version not registered".to_string(),
+                    },
+                    version.as_span(),
+                ))
+            }
+        } else {
+            Err(error::Error::new_from_span(
+                error::ErrorVariant::<Rule>::CustomError {
+                    message: "import not registered".to_string(),
+                },
+                name.as_span(),
+            ))
+        }
+    }
+
+    fn process_object(&mut self, pair: Pair<Rule>) -> Result<(), error::Error<Rule>> {
+        let mut pairs = pair.into_inner();
+        let type_name = pairs.next().unwrap();
+        let _body = pairs.next().unwrap();
+
+        if let Some(generator) = self.in_scope.iter().find_map(|gen| gen(type_name.as_str())) {
+            println!("{:?}", generator);
+            Ok(())
+        } else {
+            Err(error::Error::new_from_span(
+                error::ErrorVariant::<Rule>::CustomError {
+                    message: "Object type not in scope".to_string(),
+                },
+                type_name.as_span(),
+            ))
+        }
     }
 }
