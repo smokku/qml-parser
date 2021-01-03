b70b4ab7bdff138d56830ccae8e63e9bd231466f
Tomasz Sterna <tomek@sterna.link>
2021-01-03 17:21:26 +0100

    Store Commands reference in parser's Generator

--- bevy_qml/examples/simple.rs
+++ bevy_qml/examples/simple.rs
@@ -1,8 +1,10 @@
+/* Creating separate module for fake parser, that does not know anything about Bevy. */
 mod parser {
     pub struct Parser<'a> {
         generators: Vec<&'a mut dyn Generator>,
     }
 
+    // Parser skeleton, that uses registered generators during parsing
     impl<'a> Parser<'a> {
         pub fn new() -> Self {
             Self {
@@ -16,17 +18,19 @@ mod parser {
 
         pub fn process(&mut self, _content: &str) -> Result<(), String> {
             for gen in &mut self.generators {
-                gen.create("Object");
+                gen.create("Object"); // Let's assume we parsed "Object" symbol
             }
             Ok(())
         }
     }
 
+    // Generator trait to be implemented by user
     pub trait Generator {
         fn create(&mut self, name: &str);
     }
 }
 
+/* Create separate module, that uses parser internals and Bevy Commands for parsing. */
 mod app {
     use super::parser::{Generator, Parser};
     use bevy::{app::ScheduleRunnerSettings, prelude::*};
@@ -35,32 +39,43 @@ mod app {
         App::build()
             .add_resource(ScheduleRunnerSettings::run_once())
             .add_plugins(MinimalPlugins)
+            .add_system_to_stage(stage::FIRST, count_entities.system())
             .add_system(test_parser.system())
+            .add_system_to_stage(stage::LAST, count_entities.system())
             .run();
     }
 
-    fn test_parser() {
-        let file = "Object {}";
+    fn test_parser(commands: &mut Commands) {
+        let file = "Object {}"; // fake file content
         let mut parser = Parser::new();
-        let mut generator = AppGenerator {};
+        let mut generator = AppGenerator { commands };
         parser.register_generator(&mut generator);
         if let Err(e) = parser.process(file) {
-            eprintln!("QML parsing error:\n{}", e);
+            eprintln!("Parsing error:\n{}", e);
             std::process::exit(1);
         }
 
-        println!("parsed");
+        println!("Parsing successful");
     }
 
-    struct AppGenerator {}
+    fn count_entities(world: &mut World, _resources: &mut Resources) {
+        println!("entities: {}", world.iter().count());
+    }
 
-    impl Generator for AppGenerator {
+    struct AppGenerator<'a> {
+        commands: &'a mut Commands,
+    }
+
+    impl Generator for AppGenerator<'_> {
         fn create(&mut self, name: &str) {
             if name == "Object" {
                 println!("Creating Object");
+                self.commands.spawn((Object {},));
             }
         }
     }
+
+    struct Object {}
 }
 
 fn main() {
